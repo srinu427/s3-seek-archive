bbe9557beb5f60dd412eb9b5fc4edd85fcc75591
Srinivas Adigopula <sadigopu@cisco.com>
2024-06-05 10:55:30 +0530

    add local reader in py

--- python-reader/s4_reader.py
+++ python-reader/s4_reader.py
@@ -30,7 +30,7 @@ def parse_s4a_db(db_file):
 class S4AReaderS3:
     s3_client: Any
     bucket_name: str
-    object_name: str
+    blob_key_name: str
     entry_map: dict[str, S4AEntryMetadata]
 
     def get_file(self, name: str):
@@ -39,7 +39,7 @@ class S4AReaderS3:
             try:
                 s3_get_resp = self.s3_client.get_object(
                     Bucket=self.bucket_name,
-                    Key=self.object_name.replace(".s4a.db", ".s4a.blob"),
+                    Key=self.blob_key_name,
                     Range=f"bytes={entry_info.offset}-{entry_info.offset + entry_info.size - 1}"
                 )
                 s3_object_data = s3_get_resp['Body'].read()
@@ -68,4 +68,36 @@ def make_s4a_reader_s3(s3_client: Any, bucket_name: str, object_name: str):
     except Exception as e:
         print(f"error parsing .s4a.db: {e}")
         return None
-    return S4AReaderS3(s3_client, bucket_name, object_name, entry_map)
+    return S4AReaderS3(s3_client, bucket_name, object_name.replace(".s4a.db", ".s4a.blob"), entry_map)
+
+
+@dataclass
+class S4AReaderLocal:
+    blob_path: str
+    entry_map: dict[str, S4AEntryMetadata]
+
+    def get_file(self, name: str):
+        if name in self.entry_map:
+            entry_info = self.entry_map[name]
+            try:
+                with open(self.blob_path, 'rb') as fr:
+                    fr.seek(entry_info.offset)
+                    compressed_data = fr.read(entry_info.size)
+            except Exception as e:
+                print(f"error getting file from blob: {e}")
+                return None
+            try:
+                uncompressed_data = lzma.decompress(compressed_data)
+            except Exception as e:
+                print(f"error un-compressing data: {e}")
+                return None
+            return uncompressed_data
+
+
+def make_s4a_reader_local(db_path: str):
+    try:
+        entry_map = parse_s4a_db(db_path)
+    except Exception as e:
+        print(f"error parsing .s4a.db: {e}")
+        return None
+    return S4AReaderLocal(db_path.replace(".s4a.db", ".s4a.blob"), entry_map)
